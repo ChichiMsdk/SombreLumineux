326af6aaf1c954c30a42dae4da738d14d1fd2cde
ChichiMsdk <chihab.moussadyk@gmail.com>
2026-04-10 10:13:11 +0200

    modified:   first.c

--- first.c
+++ first.c
@@ -2,7 +2,13 @@ int main()
 {
     *(unsigned int*)0x04000000 = 0x0403;
 
-    ((unsigned short*)0x06000000)[120+80*240] = 0x001F;
+    ((unsigned short*)0x06000000)[120+80*240] = 0x101F;
+    ((unsigned short*)0x06000000)[121+80*240] = 0x101F;
+    ((unsigned short*)0x06000000)[122+80*240] = 0x101F;
+    ((unsigned short*)0x06000000)[123+80*240] = 0x101F;
+    ((unsigned short*)0x06000000)[124+80*240] = 0x101F;
+    ((unsigned short*)0x06000000)[125+80*240] = 0x101F;
+    ((unsigned short*)0x06000000)[126+80*240] = 0x101F;
     ((unsigned short*)0x06000000)[136+80*240] = 0x03E0;
     ((unsigned short*)0x06000000)[120+96*240] = 0x7C00;
 
